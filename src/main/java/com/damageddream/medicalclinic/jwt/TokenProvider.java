4008c38e9f3e2a380e8f44ac394fa0eff7b32506
Damageddream <marcan.grab@gmail.com>
2024-03-29 15:58:46 +0100

    MED-19

--- src/main/java/com/damageddream/medicalclinic/jwt/TokenProvider.java
+++ src/main/java/com/damageddream/medicalclinic/jwt/TokenProvider.java
@@ -1,10 +1,19 @@
 package com.damageddream.medicalclinic.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 @Component
 public class TokenProvider implements Serializable {
 
-    @Value
+    @Value("${app.security.jwt.secret:secret}")
+    private String secret;
+
+    UsernamePasswordAuthenticationToken getAuthentication(final String token,
+                                                          final UserDetails userDetails) {
+        final JwtParser
+    }
 }
