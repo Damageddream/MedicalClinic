7ef193552c1975ec8752accb1e436d18a46018dd
Damageddream <marcan.grab@gmail.com>
2024-03-30 09:52:34 +0100

    MED-19

--- src/main/java/com/damageddream/medicalclinic/rest/DoctorRestController.java
+++ src/main/java/com/damageddream/medicalclinic/rest/DoctorRestController.java
@@ -5,6 +5,12 @@ import com.damageddream.medicalclinic.dto.FacilityDTO;
 import com.damageddream.medicalclinic.dto.GetIdCommand;
 import com.damageddream.medicalclinic.dto.NewDoctorDTO;
 import com.damageddream.medicalclinic.service.DoctorServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,32 +24,83 @@ public class DoctorRestController {
 
     private final DoctorServiceImpl doctorServiceImpl;
 
+    @Operation(summary = "Get doctor by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the doctor",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Doctor not found",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public DoctorDTO getDoctor(@PathVariable Long id) {
         return doctorServiceImpl.findById(id);
     }
 
+    @Operation(summary = "Get facilities by doctor")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return a list of facilities by doctor",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = FacilityDTO.class)))}),
+            @ApiResponse(responseCode = "404", description = "Doctor not found",
+                    content = @Content)})
     @GetMapping("/{id}/facilities")
     public List<FacilityDTO> getDoctorsFacilities(@PathVariable Long id) {
         return doctorServiceImpl.findFacilitiesByDoctor(id);
     }
 
+    @Operation(summary = "Add a new doctor")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Doctor created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "409", description = "Doctor with that email already exists",
+                    content = @Content)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public DoctorDTO addDoctor(@RequestBody NewDoctorDTO doctor) {
         return doctorServiceImpl.save(doctor);
     }
 
+    @Operation(summary = "Add facility to doctor")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Facility added to doctor",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Doctor not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Facility not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "409", description = "Facility already is this doctor employer",
+                    content = @Content)
+    })
     @PutMapping("/{id}/facilities")
     public DoctorDTO addFacility(@PathVariable Long id, @RequestBody GetIdCommand entityId) {
         return doctorServiceImpl.addFacilityToDoctor(id, entityId);
     }
 
+    @Operation(summary = "Delete doctor")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Doctor deleted",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Doctor not found",
+                    content = @Content)
+    })
     @DeleteMapping("/{id}")
     public DoctorDTO deleteDoctor(@PathVariable Long id) {
         return doctorServiceImpl.deleteDoctor(id);
     }
 
+    @Operation(summary = "Edit doctor")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Doctor edited",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DoctorDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Doctor not found",
+                    content = @Content),
+    })
     @PutMapping("/{id}")
     public DoctorDTO updateDoctor(@PathVariable Long id, @RequestBody NewDoctorDTO doctor) {
         return doctorServiceImpl.update(id, doctor);
--- src/main/java/com/damageddream/medicalclinic/rest/PatientRestController.java
+++ src/main/java/com/damageddream/medicalclinic/rest/PatientRestController.java
@@ -5,7 +5,11 @@ import com.damageddream.medicalclinic.dto.NewPatientDTO;
 import com.damageddream.medicalclinic.dto.PatientDTO;
 import com.damageddream.medicalclinic.service.PatientService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,33 +22,84 @@ import java.util.List;
 public class PatientRestController {
 
     private final PatientService patientService;
-    @Operation(summary = "Get list of all patients")
+
+    @Operation(summary = "Get all patients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return a list of patients",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = PatientDTO.class)))}),
+            @ApiResponse(responseCode = "404", description = "Patients not found",
+                    content = @Content)})
     @GetMapping
     public List<PatientDTO> findAll() {
         return patientService.findAll();
     }
 
+    @Operation(summary = "Get patient by email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the patient",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Patient not found",
+                    content = @Content)
+    })
     @GetMapping("/{patientEmail}")
     public PatientDTO getPatient(@PathVariable String patientEmail) {
         return patientService.findByEmail(patientEmail);
     }
 
+    @Operation(summary = "Add a new patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Patient created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "409", description = "Patient with that email already exists",
+                    content = @Content)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PatientDTO addPatient(@RequestBody NewPatientDTO patient) {
         return patientService.save(patient);
     }
 
+    @Operation(summary = "Edit patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patient edited",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "409", description = "New email is not available",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Changes to Id Card No are forbidden",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "All fields of new Patient must be completed",
+                    content = @Content)
+    })
     @PutMapping("/{patientEmail}")
     public PatientDTO updatePatient(@PathVariable String patientEmail, @RequestBody NewPatientDTO patient) {
         return patientService.update(patientEmail, patient);
     }
 
+    @Operation(summary = "Edit patient password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Password edited",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Patient not found",
+                    content = @Content)
+    })
     @PatchMapping("/{patientEmail}")
     public PatientDTO updatePassword(@PathVariable String patientEmail, @RequestBody ChangePasswordCommand password) {
         return patientService.editPassword(password, patientEmail);
     }
 
+    @Operation(summary = "Delete patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patient deleted",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Patient not found",
+                    content = @Content)
+    })
     @DeleteMapping("/{patientEmail}")
     public PatientDTO deletePatient(@PathVariable String patientEmail) {
         return patientService.delete(patientEmail);
